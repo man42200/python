7555dd19fa315f6107eb5a0362f8b1bfd662a1a1
man42200 <24BIT062@sot.pdpu.ac.in>
2025-01-20 18:45:46 +0530

    Delete p 23.py

--- p 23.py
+++
@@ -1,8 +0,0 @@
-
-subject1 = float(input("Enter marks for subject 1: "))
-subject2 = float(input("Enter marks for subject 2: "))
-subject3 = float(input("Enter marks for subject 3: "))
-total = subject1 + subject2 + subject3
-average = total / 3
-print("Total marks: ", total)
-print("Average marks: ", average)
