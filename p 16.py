fe9864305f8d1d10b5db03a64e0efd341e5d5bc4
man42200 <24BIT062@sot.pdpu.ac.in>
2025-01-20 18:44:35 +0530

    Delete p 16.py

--- p 16.py
+++
@@ -1,6 +0,0 @@
-
-principal = float(input("Enter the principal amount: "))
-rate = float(input("Enter the rate of interest: "))
-time = float(input("Enter the time period in years: "))
-interest = (principal * rate * time) / 100
-print(f"The calculated interest is: {interest}")
